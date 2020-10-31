8f32da0e3439da03c5a650b2ebc29bce278078ae
michelle-lo <mlo20@stuy.edu>
2020-10-31 15:42:51 -0400

    getArea done. classify on the way

--- Tester.java
+++ Tester.java
@@ -1,14 +1,36 @@
 public class Tester {
   public static void main (String[] args) {
+    //triangle 2
     Point p1 = new Point (1, 2);
     Point p2 = new Point (3, 4);
-    Point p3 = new Point (1, 2);
+    Point p1_2 = new Point (1, 2);
+    Point p3 = new Point (4, 2);
+    //triangle 2
+    Point p4 = new Point (5,6);
+    Point p5 = new Point (10,5);
+    Point p6 = new Point (10,10);
 
     System.out.println("----distanceTo----");
     System.out.println(p1.distanceTo(p2)); //2.828
     System.out.println("----equals----");
     System.out.println(p1.equals(p2)); //false
-    System.out.println(p1.equals(p3)); //true
+    System.out.println(p1.equals(p1_2)); //true
+
+
+    Triangles t1 = new Triangles (p1, p2, p3);
+    Triangles t2 = new Triangles (p4, p5, p6);
+    //Triangle t2 = new Triangle (3, 4);
+    //Triangle t3 = new Triangle (4, 2);
+    System.out.println("----getPerimeter----");
+    System.out.println(t1.getPerimeter()); //8.064
+    System.out.println(t2.getPerimeter()); //16.502
+    System.out.println("----getArea----");
+    System.out.println(t1.getArea()); //around 3
+    System.out.println(t2.getArea()); //12.5
+
+
+
+
   }
   //public static String printcheck (String name, Object actual, Object expected) {
 
--- Triangles.java
+++ Triangles.java
@@ -1,28 +1,49 @@
 public class Triangles {
-  private v1, v2, v3;
+  private Point v1, v2, v3;
 
-  public Triangle(Point a, Point b, Point c) {
+  public Triangles(Point a, Point b, Point c) {
     v1 = a;
     v2 = b;
     v3 = c;
   }
 
-  public Triangle(double x1, double y1,double x2, double y2,double x3, double y3) {
+  public Triangles(double x1, double y1,double x2, double y2,double x3, double y3) {
     v1 = new Point (x1, y1);
     v2 = new Point (x2, y2);
     v3 = new Point (x3, y3);
   }
 
+  //Return the perimeter without any rounding.
+  public double getPerimeter() {
+    double a = v1.distanceTo(v2);
+    double b = v2.distanceTo(v3);
+    double c = v3.distanceTo(v1);
+    return a + b + c;
+  }
 
-  /*
-  a) Three points.
- private Point v1, v2, v3;
+  //Return the area using Heron's formula without any rounding.
+  public double getArea() {
+    double a = v1.distanceTo(v2);
+    double b = v2.distanceTo(v3);
+    double c = v3.distanceTo(v1);
 
- b) Two constructors constructors :
-  public Triangle(Point a, Point b, Point c)
-     Points are immutable, so you can just store the reference, no copy needed.
+    double semiP = getPerimeter() / 2;
 
-  public Triangle(double x1, double y1,double x2, double y2,double x3, double y3)
-    Make new points out of the doubles to store in the instance variables.
-    */
+    return Math.sqrt((semiP * (semiP - a) * (semiP - b) * (semiP - c)));
+  }
+
+  //Return the "equilateral" "isosceles" or "scalene"
+  //Round distances to the nearest tenthousandths for your classifications.
+  public String classify() {
+    double A = (Math.round(v1.distanceTo(v2) * 10000)) / 10000.0;
+    return "" + A;
+  }
+
+/*
+  public String toString()
+       The format should be "v1(23.0, 4.0) v2(-2.3, 5.001) v3(5.0, 0.52)"
+  */
+
+
+
 }
